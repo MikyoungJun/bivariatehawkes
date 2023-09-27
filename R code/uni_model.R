b6ce125b9306e9aace1879e36e58e9b7d97a8ae4
MikyoungJun <145954103+MikyoungJun@users.noreply.github.com>
2023-09-26 23:43:08 -0500

    Create uni_model.R

---
+++ R code/uni_model.R
@@ -0,0 +1,7 @@
+####################################
+### updated: 09/25/23
+### by Mikyoung Jun
+####################################
+
+## Note: this file contains codes to fit M1-3, but minor modification would be needed (in places commented below) to fit other M1-x models 
+## 
