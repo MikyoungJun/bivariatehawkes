560cf1fb85400bde29698635deb2b034504c0606
MikyoungJun <145954103+MikyoungJun@users.noreply.github.com>
2023-09-27 00:05:55 -0500

    Update data_process.R

--- data/data_process.R
+++ data/data_process.R
@@ -1,1 +1,21 @@
+######## Last updated 09/26/23
+######## by Mikyoung Jun
+
+######## This file contains codes to process terrorism data from GTD, and covariates used from other sources in order to prepare them in a format 
+########  to be used for the analysis
+
+
+
+####### Terrorism data from GTD ########
+
+### Afghanistan ###
+
+
+### Nigeria ###
+
+###### population from WorldPop #######
+
+
+###### elevation ########
+
 
