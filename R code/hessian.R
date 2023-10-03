e3e1c52076e4711eeba4d2faf42d4b16e1292bb6
MikyoungJun <145954103+MikyoungJun@users.noreply.github.com>
2023-10-03 15:58:14 -0500

    Update hessian.R

--- R code/hessian.R
+++ R code/hessian.R
@@ -5,7 +5,12 @@
 ### However, calculation of Jacobian for some of the parameters is quite complex. Therefore, once we obtain parameter estimates, we use another R package to calculate 
 ### Hessian matrices as illustrated below.
 
+## First, rewrite the (negative) logLiklelihood function in bi_model.R (or univ_model.R for univariate problem) WITHOUT parameter transformation 
+## Let us call such function logLik1
+## save the values of parameter estimates in "par" 
 
 library(numDeriv)
 
-hessian(fun
+Hes= hessian(func=logLik1, x=par) ## Hessian matrix of negative loglikelihood function evaluated at the parameter estimates
+
+sqrt(diag(solve(Hes))) ## this should give you the asymptotic standard errors of parameter estimates 
